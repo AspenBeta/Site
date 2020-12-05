8b9fa871a3c46020d81ba93f703fb71ad03a8242
AspenBeta <75514888+AspenBeta@users.noreply.github.com>
2020-12-04 23:12:09 -0600

    Add files via upload

---
+++ Aspen/code.php
@@ -0,0 +1,23 @@
+<?php
+$action = $_GET["action"];
+$Username = $_POST["Username"];
+$Password = $_POST["Password"];
+
+if($action = "save") {
+  file_put_contents("saves.txt", $Username);
+  file_put_contents("saves.txt", $Password);
+}
+?>
+<html>
+<head>
+ <title>myform</title>
+</head>
+<body>
+  <form action="?action=save" name="myform" method="post">
+    <input type=text name="Username">
+	<input type=text name="Password">
+    <input type="submit" value="submit">
+  </form>
+  <?php include 'code.php';?>
+</body>
+</html>
